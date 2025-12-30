0b3d9935254b464f77bd3e342b788608b635e80f
Yannick Wenger <y.wenger@gmx.de>
2025-12-31 00:00:26 +0100

    Add fish shell configuration

---
+++ fish/functions/cloud.fish
@@ -0,0 +1,3 @@
+function cloud --wraps=cd\ \~/Library/Mobile\\\ Documents/com\~apple\~CloudDocs/ --description alias\ cloud\ cd\ \~/Library/Mobile\\\ Documents/com\~apple\~CloudDocs/
+  cd ~/Library/Mobile\ Documents/com~apple~CloudDocs/ $argv;
+end
